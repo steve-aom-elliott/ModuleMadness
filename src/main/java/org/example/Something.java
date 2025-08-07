fa96b6277aeeb30559734e3a2eaaaf76a9d0d6a1
Steve Elliott <steve@moderne.io>
2025-08-07 16:16:19 -0400

    Adding a file that depends on generated code.

---
+++ src/main/java/org/example/Something.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import io.modernesteve.client.github.api.DefaultApi;
+
+public class Something {
+    DefaultApi defaultApi;
+
+    public Something() {
+        defaultApi = new DefaultApi();
+    }
+}
